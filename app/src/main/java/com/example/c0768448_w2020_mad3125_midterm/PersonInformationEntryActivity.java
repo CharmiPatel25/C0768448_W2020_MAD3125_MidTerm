d87447947dbfba94bd1b7f522881ec617dfda5bd
Charmi <c0768448@mylambton.ca>
2020-04-07 21:37:44 -0400

    Add PersonInformationEntry Activity

---
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/PersonInformationEntryActivity.java
@@ -0,0 +1,14 @@
+package com.example.c0768448_w2020_mad3125_midterm;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class PersonInformationEntryActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_person_information_entry);
+    }
+}
