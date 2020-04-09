752e9ed219c25a55eccfef012815d3be33b5d84d
Charmi <c0768448@mylambton.ca>
2020-04-08 23:12:33 -0400

    Add gender selction logic

--- app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
@@ -1,8 +1,12 @@
 package com.example.c0768448_w2020_mad3125_midterm.UI;
 
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+
 import android.app.DatePickerDialog;
+import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
 import android.text.InputType;
 import android.view.View;
@@ -12,9 +16,15 @@ import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.example.c0768448_w2020_mad3125_midterm.R;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.time.LocalDate;
+import java.time.Period;
+import java.time.format.DateTimeFormatter;
 import java.util.Calendar;
 import java.util.Date;
 import java.util.Locale;
@@ -27,6 +37,11 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
     private RadioGroup rdGender;
     private Button btnCalculate;
     private   DatePickerDialog picker;
+    final Calendar cldr = Calendar.getInstance();
+    final int day = cldr.get(Calendar.DAY_OF_MONTH);
+    final   int month = cldr.get(Calendar.MONTH);
+    final int year = cldr.get(Calendar.YEAR);
+    String gender=null;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -45,13 +60,9 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
         rdMrs = findViewById(R.id.rdFeMale);
         rdOther = findViewById(R.id.rdOther);
         btnCalculate = findViewById(R.id.btnCalculate);
-        final Calendar cldr = Calendar.getInstance();
-       final int day = cldr.get(Calendar.DAY_OF_MONTH);
-      final   int month = cldr.get(Calendar.MONTH);
-       final int year = cldr.get(Calendar.YEAR);
+
 
         txtTaxFillDate.setText(new StringBuilder()
-                // Month is 0 based, just add 1
                 .append(day).append(" ").append("-").append(month + 1).append("-")
                 .append(year));
         //Birth Date Picker
@@ -76,6 +87,48 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
         });
 
 
+        rdGender.setOnCheckedChangeListener(new RadioGroup.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(RadioGroup radioGroup, int i) {
+
+                if(i == R.id.rdMale){
+                    gender = rdMr.getText().toString();
+                }else if(i == R.id.rdFeMale){
+                    gender = rdMrs.getText().toString();
+                }else {
+                    gender = rdOther.getText().toString();
+                }
+            }
+
+        });
+
+        btnCalculate.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.O)
+            @Override
+            public void onClick(View v) {
+                int age;
+                age = calculateAge(txtDob.getText().toString());
+                if (age < 18) {
+                    btnCalculate.setAlpha(.5f);
+                    btnCalculate.setClickable(false);
+                } else {
+
+                    Intent intent = new Intent(PersonInformationEntryActivity.this, TaxDataDetailsActivity.class);
+                    startActivity(intent);
+                }
+            }
+        });
 
     }
+    int calculateAge(String date){
+        Calendar dob = Calendar.getInstance();
+        Calendar today = Calendar.getInstance();
+        int age = today.get(Calendar.YEAR) - dob.get(Calendar.YEAR);
+
+        if(today.get(Calendar.DAY_OF_MONTH) < dob.get(Calendar.DAY_OF_MONTH)){
+            age--;
+        }
+        return age;
+    }
+
 }
