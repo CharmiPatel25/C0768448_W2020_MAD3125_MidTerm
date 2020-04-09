998d966ea5c5ab6bff88ec9bc68cadf53018bed5
Charmi <c0768448@mylambton.ca>
2020-04-08 20:16:07 -0400

    Add BirthDate picker

--- app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
@@ -2,14 +2,22 @@ package com.example.c0768448_w2020_mad3125_midterm.UI;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
+import android.text.InputType;
+import android.view.View;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 import android.widget.TextView;
 
 import com.example.c0768448_w2020_mad3125_midterm.R;
+
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class PersonInformationEntryActivity extends AppCompatActivity {
 
@@ -18,7 +26,7 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
     private RadioButton rdMr,rdMrs,rdOther;
     private RadioGroup rdGender;
     private Button btnCalculate;
-
+    private   DatePickerDialog picker;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -37,6 +45,29 @@ public class PersonInformationEntryActivity extends AppCompatActivity {
         rdMrs = findViewById(R.id.rdFeMale);
         rdOther = findViewById(R.id.rdOther);
         btnCalculate = findViewById(R.id.btnCalculate);
+
+        //Birth Date Picker
+        txtDob.setInputType(InputType.TYPE_NULL);
+        txtDob.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                final Calendar cldr = Calendar.getInstance();
+                int day = cldr.get(Calendar.DAY_OF_MONTH);
+                int month = cldr.get(Calendar.MONTH);
+                int year = cldr.get(Calendar.YEAR);
+                // date picker dialog
+                picker = new DatePickerDialog(PersonInformationEntryActivity.this,
+                        new DatePickerDialog.OnDateSetListener() {
+
+
+                            @Override
+                            public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+                                txtDob.setText(dayOfMonth + "/" + (monthOfYear + 1) + "/" + year);
+                            }
+                        }, year, month, day);
+                picker.show();
+            }
+        });
 
 
     }
