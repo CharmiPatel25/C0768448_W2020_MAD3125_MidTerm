609f1caf84ed00e8e41bf9a6df64ecbbc9e11bed
Charmi <c0768448@mylambton.ca>
2020-04-08 20:02:25 -0400

    FindViewBy ID for Entry fields

--- app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/PersonInformationEntryActivity.java
@@ -3,14 +3,41 @@ package com.example.c0768448_w2020_mad3125_midterm.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 import com.example.c0768448_w2020_mad3125_midterm.R;
 
 public class PersonInformationEntryActivity extends AppCompatActivity {
 
+    private EditText txtSin,txtFirstName,txtLastName,txtDob,txtTaxFillDate,txtGrossIncome,txtRrsContri;
+    private  TextView txtAge;
+    private RadioButton rdMr,rdMrs,rdOther;
+    private RadioGroup rdGender;
+    private Button btnCalculate;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person_information_entry);
+
+        txtSin = findViewById(R.id.txtSinNumb);
+        txtFirstName = findViewById(R.id.txtFirstName);
+        txtLastName = findViewById(R.id.txtLastName);
+        txtDob = findViewById(R.id.txtBirthDate);
+        txtTaxFillDate = findViewById(R.id.txtTaxFillDate);
+        txtGrossIncome = findViewById(R.id.txtGrossIncome);
+        txtRrsContri = findViewById(R.id.txtRrs);
+        txtAge = findViewById(R.id.txtAge);
+        rdGender = findViewById(R.id.rdGrpGender);
+        rdMr = findViewById(R.id.rdMale);
+        rdMrs = findViewById(R.id.rdFeMale);
+        rdOther = findViewById(R.id.rdOther);
+        btnCalculate = findViewById(R.id.btnCalculate);
+
+
     }
 }
