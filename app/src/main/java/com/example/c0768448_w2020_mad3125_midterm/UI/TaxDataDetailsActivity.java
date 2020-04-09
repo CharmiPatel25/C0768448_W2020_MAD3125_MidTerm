598c34b01629da27c87f96452e7a872a33a86265
Charmi <c0768448@mylambton.ca>
2020-04-09 06:13:14 -0400

    Initialize parametrs in Tax Details Display

--- app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/TaxDataDetailsActivity.java
+++ app/src/main/java/com/example/c0768448_w2020_mad3125_midterm/UI/TaxDataDetailsActivity.java
@@ -3,11 +3,18 @@ package com.example.c0768448_w2020_mad3125_midterm.UI;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
+import com.example.c0768448_w2020_mad3125_midterm.Models.CRACustomer;
 import com.example.c0768448_w2020_mad3125_midterm.R;
 
 public class TaxDataDetailsActivity extends AppCompatActivity {
+    CRACustomer customer;
+    private TextView lblSin,lblfullName,lblBirthDate,lblGender,lblAge,lblTaxFillingDate,lblGrossIncome,lblRRsContri,lblRrsCarry,lblFederal,lblProvincial,lblEi,
+    lblCpp,lblTaxableIncome,lblTaxPaid;
 
+    double cpp = 0, ei = 0;  double rrsp = 0, rrspCf = 0, taxableIncome, federalTax,
+            provincialTax, totalTaxPaid;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
